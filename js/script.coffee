31d763a30d458d335c0e940ac1989640ec896079
Carlos Rodriguez <me@carlosrodriguez.us>
2012-02-05 12:40:32 -0500

    Made some fixes for Coffeescript.

--- js/script.coffee
+++ js/script.coffee
@@ -1,44 +1,60 @@
-$ = require('jquery')
-$ ->
+compare = ->
+  if not checkcompare() then alert "You need to select at least two products"
+  else setvalues()
 
-  compare = ->
-    if not checkcompare() then alert "You need to select at least two products"
-    else setvalues()
+setvalues = ->
+  removevalue()
+  $(".compare-check:checked").each (i, element) => storevalue($(element).attr('id'))
 
-  setvalues = ->
-    removevalue()
-    $(".compare-check:checked").each -> storeValue($(this).attr('id'))
+getcount = ->
+  $(".compare-check:checked").length
 
-  setcomparebutton = ->
-    $button = $(".compare-button")
-    if getCount() <= 1 then $button.off('click', compare).addClass('compare-button-disabled') else $button.on('click', compare).removeClass('compare-button-disabled')
+setcomparebutton = ->
+  $button = $(".compare-button")
+  if getcount() <= 1 
+    $button.attr('disabled', true)
+    $button.off('click', compare).addClass('compare-button-disabled')
+  else
+    $button.attr('disabled', false)
+    $button.on('click', compare).removeClass('compare-button-disabled')
 
-  getcount = ->
-    $(".compare-check:checked").length
+checkcompare = ->
+  if getcount() >= 2 then true else false
 
-  checkcompare = ->
-    (getCount() >= 2) ? true : false
+addinput = ->
+  m = setinput()
+  $('body').append(m)
 
-  setsorage = ->
-    if not $("#hdItemIDs").length then  setinput().appendTo('body') else $("#hdItemIDs")
+setstorage = ->
+  if not $("#hdItemIDs").length then addinput() else $("#hdItemIDs")
 
-  setinput = ->
-    $input = $("<input>")
-    $input.attr =
-      name: "hdItemIDs"
-      id: "hdItemIDs"
-      type: "hidden"
+setinput = ->
+  $input = $("<input>")
+  $input.attr(
+    name: "hdItemIDs"
+    id: "hdItemIDs"
+    type: "hidden"
+  )
 
-  storevalue = ->
-    $input = $("#hdItemIDs")
-    content = (($input.val() == "") || ($input.val() == "temp") || ($input.val() == "littelfuse")) ? id : $input.val() + "," + id
+storevalue = (id) ->
+  $input = $("#hdItemIDs")
+  c = $input.val()
+  content = if c == "" then id else $input.val() + "," + id
+  $input.val(content);
 
-  removevalue = ->
-    $("#hdItemIDs").val('')
+removevalue = ->
+  $("#hdItemIDs").val('')
 
-  $(".compare-check").live('click') ->
-    if $(this).attr("checked") && getcount() > 4 then $(this).attr("checked", false) alert 'Max items'
-    setcomparebutton()
+alertitems = ($item) ->
+  alert 'Max items'
+  $item.attr("checked", false)
 
+$(".compare-check").live 'click', (event) ->
+  if $(this).attr("checked") && getcount() > 4 then alertitems($(this)) else setvalues()
   setcomparebutton()
-  setstorage()
+
+$(".compare-button").live 'click', (event) ->
+  compare()
+
+setcomparebutton()
+setstorage()
--- js/script.js
+++ js/script.js
@@ -1,75 +1,102 @@
 (function() {
-  var $;
+  var addinput, alertitems, checkcompare, compare, getcount, removevalue, setcomparebutton, setinput, setstorage, setvalues, storevalue;
 
-  $ = require('jquery');
+  compare = function() {
+    if (!checkcompare()) {
+      return alert("You need to select at least two products");
+    } else {
+      return setvalues();
+    }
+  };
 
-  $(function() {
-    var checkcompare, compare, getcount, removevalue, setcomparebutton, setinput, setsorage, setvalues, storevalue;
-    compare = function() {
-      if (!checkcompare()) {
-        return alert("You need to select at least two products");
-      } else {
-        return setvalues();
-      }
-    };
-    setvalues = function() {
-      removevalue();
-      return $(".compare-check:checked").each(function() {
-        return storeValue($(this).attr('id'));
-      });
-    };
-    setcomparebutton = function() {
-      var $button;
-      $button = $(".compare-button");
-      if (getCount() <= 1) {
-        return $button.off('click', compare).addClass('compare-button-disabled');
-      } else {
-        return $button.on('click', compare).removeClass('compare-button-disabled');
-      }
-    };
-    getcount = function() {
-      return $(".compare-check:checked").length;
-    };
-    checkcompare = function() {
-      var _ref;
-      return (_ref = getCount() >= 2) != null ? _ref : {
-        "true": false
-      };
-    };
-    setsorage = function() {
-      if (!$("#hdItemIDs").length) {
-        return setinput().appendTo('body');
-      } else {
-        return $("#hdItemIDs");
-      }
-    };
-    setinput = function() {
-      var $input;
-      $input = $("<input>");
-      return $input.attr = {
-        name: "hdItemIDs",
-        id: "hdItemIDs",
-        type: "hidden"
-      };
-    };
-    storevalue = function() {
-      var $input, content, _ref;
-      $input = $("#hdItemIDs");
-      return content = (_ref = ($input.val() === "") || ($input.val() === "temp") || ($input.val() === "littelfuse")) != null ? _ref : {
-        id: $input.val() + "," + id
-      };
-    };
-    removevalue = function() {
-      return $("#hdItemIDs").val('');
-    };
-    $(".compare-check").live('click')(function() {
-      if ($(this).attr("checked") && getcount() > 4) {
-        $(this).attr("checked", false)(alert('Max items'));
-      }
-      return setcomparebutton();
+  setvalues = function() {
+    var _this = this;
+    removevalue();
+    return $(".compare-check:checked").each(function(i, element) {
+      return storevalue($(element).attr('id'));
     });
-    setcomparebutton();
-    return setstorage();
+  };
+
+  getcount = function() {
+    return $(".compare-check:checked").length;
+  };
+
+  setcomparebutton = function() {
+    var $button;
+    $button = $(".compare-button");
+    if (getcount() <= 1) {
+      $button.attr('disabled', true);
+      return $button.off('click', compare).addClass('compare-button-disabled');
+    } else {
+      $button.attr('disabled', false);
+      return $button.on('click', compare).removeClass('compare-button-disabled');
+    }
+  };
+
+  checkcompare = function() {
+    if (getcount() >= 2) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  addinput = function() {
+    var m;
+    m = setinput();
+    return $('body').append(m);
+  };
+
+  setstorage = function() {
+    if (!$("#hdItemIDs").length) {
+      return addinput();
+    } else {
+      return $("#hdItemIDs");
+    }
+  };
+
+  setinput = function() {
+    var $input;
+    $input = $("<input>");
+    return $input.attr({
+      name: "hdItemIDs",
+      id: "hdItemIDs",
+      type: "hidden"
+    });
+  };
+
+  storevalue = function(id) {
+    var $input, c, content;
+    $input = $("#hdItemIDs");
+    c = $input.val();
+    content = c === "" ? id : $input.val() + "," + id;
+    return $input.val(content);
+  };
+
+  removevalue = function() {
+    return $("#hdItemIDs").val('');
+  };
+
+  alertitems = function($item) {
+    alert('Max items');
+    return $item.attr("checked", false);
+  };
+
+  $(".compare-check").live('click', function(event) {
+    if ($(this).attr("checked") && getcount() > 4) {
+      alertitems($(this));
+    } else {
+      setvalues();
+    }
+    return setcomparebutton();
   });
+
+  $(".compare-button").live('click', function(event) {
+    return compare();
+  });
+
+  setcomparebutton();
+
+  setstorage();
 
 }).call(this);
