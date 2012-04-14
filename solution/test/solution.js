2c1f96bcd71138bce5071e2d000dbed1005e8999
Carlos Rodriguez <me@carlosrodriguez.us>
2012-04-14 19:19:16 -0400

    Set the solution coffee script. Setting the tests

--- solution/test/solution.js
+++ solution/test/solution.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
 		
-	module("Do some sanity check");
+	module("Do some sanity check", {
+		setup: function(){
+			this.search = doSearch;
+		}
+	});
 	
 	test("Sanity Check", function() {
 		
 		//expect(#); // Set the number of tests
 
+		search = this.search;
+		console.log(search);
+
 		// Are your functions still functions?
-		//equal(typeof(functionName), "function", "functionName is a function");
+		equal(typeof(search.init), "function", "init is a function");
 		
 	});		
 	
